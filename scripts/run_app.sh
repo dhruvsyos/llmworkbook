1ec63ef68245333e6bd76ae6477e60ed87dd0b87
dhruv <dhruv166arya@gmail.com>
2025-01-07 00:17:20 +0530

    Add script to perform code checking

---
+++ scripts/run_app.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Colors for output
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+CYAN='\033[0;36m'
+RESET='\033[0m'
+
+# Function to print a separator
+print_separator() {
+  echo -e "${YELLOW}============================================================${RESET}"
+}

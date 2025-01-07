3847c575c35ca0c001dae619f5b64653af9d7b60
dhruv <dhruv166arya@gmail.com>
2025-01-08 01:06:00 +0530

    Add wrappers to process DataFrames, Arrays - 1d, 2d

--- llmworkbook/__init__.py
+++ llmworkbook/__init__.py
@@ -5,5 +5,11 @@ llmworkbook package initialization.
 from .config import LLMConfig
 from .runner import LLMRunner
 from .integrator import LLMDataFrameIntegrator
+from .wrappers import WrapDataFrame, WrapDataArray, WrapPromptList
 
-__all__ = ["LLMConfig", "LLMRunner", "LLMDataFrameIntegrator"]
+__all__ = ["LLMConfig", 
+           "LLMRunner", 
+           "LLMDataFrameIntegrator", 
+           "WrapDataFrame", 
+           "WrapDataArray", 
+           "WrapPromptList"]
